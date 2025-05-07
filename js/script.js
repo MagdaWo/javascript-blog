58a9255893a20346d2952eb94f1db7f959803bee
Magda Wołczak <listonoszmeg@gmail.com>
2025-05-07 23:11:09 +0200

    Add tags

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@ const titleClickHandler = function (event) {
 const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
+    optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
 
@@ -94,3 +95,50 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags() {
+  
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    /* START LOOP: for every article: */
+
+    for (let article of articles) {
+
+        /* find tags wrapper */
+        const tagsWrapper = document.querySelector(optArticleTagsSelector);
+        /* make html variable with empty string */
+        let html = '';
+        /* get tags from data-tags attribute */
+        const articleTags = article.getAttribute('data-tags');
+        console.log(articleTags);
+
+        /* split tags into array */
+        const articleTagsArray = articleTags.split(' ');
+        console.log(articleTagsArray)
+
+        /* START LOOP: for each tag */
+        for(let tag of articleTagsArray){
+            console.log(tag)
+        
+        /* generate HTML of the link */
+        const tagHTML = '<li><a href="#tag-' + tag + '">' + tag  + ' </a></li>';
+        console.log(tagHTML);
+
+        /* add generated code to html variable */
+        html += tagHTML;
+        console.log(html)
+
+        /* END LOOP: for each tag */
+        }
+        /* insert HTML of all the links into the tags wrapper */
+        tagsWrapper.innerHTML = html;
+        
+        /* END LOOP: for every article: */
+  
+
+    
+}
+
+}
+generateTags()
